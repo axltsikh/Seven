5e65b1249dee73c6ec9ace88cd260dca3cb749c8
axltsikh <lioshatichon@gmail.com>
2022-11-19 14:06:44 +0300

    final

--- LabSeven/src/test/java/Tests.java
+++ LabSeven/src/test/java/Tests.java
@@ -9,133 +9,77 @@ public class Tests {
         Triangle triangle=new Triangle();
     }
     @Test
-    public void Sum_2and6_Test(){
-        double a = 2.0;
-        double b = 6.0;
-        char operation = '+';
-        double waitedResult = 8.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void FirstCalc(){
+        Assert.assertEquals(Calculator.operation(2.0, 6.0, '+'), 8.0, 0);
     }
 
     @Test
-    public void Sum_435and32_Test(){
-        double a = 435.0;
-        double b = 32.0;
-        char operation = '+';
-        double waitedResult = 467.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void SecondCalc(){
+        Assert.assertEquals(Calculator.operation(435.0, 32.0, '+'), 467.0, 0);
     }
 
     @Test
-    public void Difference_5and6_Test(){
-        double a = 5.0;
-        double b = 6.0;
-        char operation = '-';
-        double waitedResult = -1.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void ThirdCalc(){
+        Assert.assertEquals(Calculator.operation(5.0, 6.0, '-'), -1.0, 0);
     }
 
     @Test
-    public void Difference_11andMinus11_Test(){
-        double a = 10.0;
-        double b = -10.0;
-        char operation = '-';
-        double waitedResult = 20.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void FourthCalc(){
+        Assert.assertEquals(Calculator.operation(10.0, -10.0, '-'), 20.0, 0);
     }
 
     @Test
-    public void Multiply_12and12_Test(){
-        double a = 12.0;
-        double b = 12.0;
-        char operation = '*';
-        double waitedResult = 144.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void FifthCalc(){
+        Assert.assertEquals(Calculator.operation(12.0, 12.0, '*'), 144.0, 0);
     }
 
     @Test
-    public void Multiply_99and99_Test(){
-        double a = 99.0;
-        double b = 99.0;
-        char operation = '*';
-        double waitedResult = 9801.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void SixthCalc(){
+        Assert.assertEquals(Calculator.operation(99.0, 99.0, '*'), 9801.0, 0);
     }
 
     @Test
-    public void Divide_3and5_Test(){
-        double a = 3.0;
-        double b = 5.0;
-        char operation = '/';
-        double waitedResult = 0.6;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void SeventhCalc(){
+        Assert.assertEquals(Calculator.operation(3.0, 5.0, '/'), 0.6, 0);
     }
 
     @Test
-    public void Divide_100and5_Test(){
-        double a = 100.0;
-        double b = 5.0;
-        char operation = '/';
-        double waitedResult = 20.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void EightCalc(){
+        Assert.assertEquals(Calculator.operation(100.0, 5.0, '/'), 20.0, 0);
     }
 
     @Test
-    public void Mod_52And5_Test(){
-        double a = 52.0;
-        double b = 5.0;
-        char operation = '%';
-        double waitedResult = 2.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void NineCalc(){
+        Assert.assertEquals(Calculator.operation(52.0, 5.0, '%'), 2.0, 0);
     }
 
     @Test
-    public void Mod_100and100_Test(){
-        double a = 100.0;
-        double b = 100.0;
-        char operation = '%';
-        double waitedResult = 0.0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult, 0);
+    public void TenCalc(){
+        Assert.assertEquals(Calculator.operation(100.0,100.0, '%'), 0.0, 0);
     }
 
     @Test
-    public void InvalidOperationTest(){
-        double a = 14;
-        double b = 77;
-        char operation = '@';
-        double waitedResult = 0;
-        Assert.assertEquals(Calculator.operation(a, b, operation), waitedResult,0);
+    public void ElevenCalc(){
+        Assert.assertEquals(Calculator.operation(14, 77, '@'), 0,0);
     }
     @Test
     public void Test1(){
-        double a = 10.0;
-        double b = 4.0;
-        double c = 5.0;
-        Assert.assertFalse(Triangle.checkTriangle(a,b,c));
+        Assert.assertFalse(Triangle.checkTriangle(10.0,4.0,5.0));
     }
 
     @Test
     public void Test2(){
-        double a = 4.0;
-        double b = 5.0;
-        double c = 6.0;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(4.0,5.0,6.0));
     }
 
     @Test
     public void Test3(){
-        double a = 100.0;
-        double b = 1.0;
-        double c = 1.0;
-        Assert.assertFalse(Triangle.checkTriangle(a,b,c));
+        Assert.assertFalse(Triangle.checkTriangle(100.0,1.0,1.0));
     }
 
     @Test
     public void Test4(){
-        double a = 4;
-        double b = 3;
-        double c = 5;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(4,3,5));
     }
 
     @Test
@@ -143,47 +87,32 @@ public class Tests {
         double a = 16.4;
         double b = 21.8;
         double c = 9.99;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(16.4,21.8,9.99));
     }
 
     @Test
     public void Test6(){
-        double a = -196.4;
-        double b = -211.8;
-        double c = 1009.99;
-        Assert.assertFalse(Triangle.checkTriangle(a,b,c));
+        Assert.assertFalse(Triangle.checkTriangle(-196.4,-211.8,1009.99));
     }
 
     @Test
     public void Test7(){
-        double a = 1;
-        double b = 1;
-        double c = 1;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(1,1,1));
     }
 
     @Test
     public void Test8(){
-        double a = 0;
-        double b = 0;
-        double c = 0;
-        Assert.assertFalse(Triangle.checkTriangle(a,b,c));
+        Assert.assertFalse(Triangle.checkTriangle(0,0,0));
     }
 
     @Test
     public void Test9(){
-        double a = 31;
-        double b = 31;
-        double c = 31;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(31,31,31));
     }
 
     @Test
     public void Test10(){
-        double a = 6;
-        double b = 8;
-        double c = 10;
-        Assert.assertTrue(Triangle.checkTriangle(a,b,c));
+        Assert.assertTrue(Triangle.checkTriangle(6,8,10));
     }
 
 }
